c05263059f43006482a25f7e38f4fcb6e8b1bbf2
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-06-25 06:58:23 +0900

    models.pyにUserManagerモデルを作成し、データを追加するコードの作成

--- myapp/models.py
+++ myapp/models.py
@@ -1,19 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # ユーザー情報
+class UserManager(BaseUserManager):
+    def create_user(self, mail, password=None, **extra_fields):
+        if not mail:
+            raise ValueError('メールアドレスは必須です')
+        user = self.model(mail=self.normalize_email(mail), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mail, password=None, **extra_fields):
+        extra_fields.setdefault('manager_flag', True)
+        user = self.create_user(mail, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
 class User(AbstractBaseUser):
     mail = models.EmailField(unique=True)
     manager_flag = models.BooleanField(default=False)  # Shop管理者かどうか
     job = models.CharField(max_length=100)
     birth_year = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'mail'
     # REQUIRED_FIELDS = ['job', 'birth_year']  # 必要に応じて
 
+    objects = UserManager()
+
     def __str__(self):
         return f"{self.mail} ({'管理者' if self.manager_flag else '一般'})"
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
 # サブスクリプションに関する情報
 # class Subscription(models.Model):
