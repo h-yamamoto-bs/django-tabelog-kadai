c695cd93ea64304fb9f3be5eddfaf957bab3ef40
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-06-30 08:58:13 +0900

    データの追加とテンプレートの手本を作成_copilot

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Shop
+
+def shop_list(request):
+    shops = Shop.objects.select_related('user').all()
+    return render(request, 'myapp/shop_list.html', {'shops': shops})
 
 # Create your views here.
