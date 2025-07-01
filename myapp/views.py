d1e2fcc9accf557c534726bbac80f98a85b0e9ad
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-07-01 23:59:16 +0900

    myappの削除

--- myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Shop
-
-def shop_list(request):
-    shops = Shop.objects.select_related('user').all()
-    return render(request, 'myapp/shop_list.html', {'shops': shops})
-
-# Create your views here.
