451e70b900bddd1d77be4f7a3c2a0361b77f09ef
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-07-01 23:41:13 +0900

    不必要なモデルをコメントアウト

--- favorites/models.py
+++ favorites/models.py
@@ -1,16 +1,16 @@
-# favorites/models.py
+# # favorites/models.py
 
-from accounts.models import User
-from shops.models import Shop
-from django.db import models
+# from accounts.models import User
+# from shops.models import Shop
+# from django.db import models
 
-class Favorite(models.Model):
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+# class Favorite(models.Model):
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=False, blank=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
 
-    class Meta:
-        db_table = 'favorites'
-        unique_together = ('shop', 'user')
+#     class Meta:
+#         db_table = 'favorites'
+#         unique_together = ('shop', 'user')
 
-    def __str__(self):
-        return f"{self.user} のお気に入り: {self.shop.name}"
+#     def __str__(self):
+#         return f"{self.user} のお気に入り: {self.shop.name}"
--- reservations/models.py
+++ reservations/models.py
@@ -1,17 +1,17 @@
-# reservations/models.py
+# # reservations/models.py
 
-from shops.models import Shop
-from accounts.models import User
-from django.db import models
+# from shops.models import Shop
+# from accounts.models import User
+# from django.db import models
 
-class Reservation(models.Model):
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    date = models.DateTimeField(null=False, blank=False)
-    number_of_people = models.PositiveIntegerField(null=False, blank=False)
+# class Reservation(models.Model):
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, null=False, blank=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+#     date = models.DateTimeField(null=False, blank=False)
+#     number_of_people = models.PositiveIntegerField(null=False, blank=False)
 
-    class Meta:
-        db_table = 'reservations'
+#     class Meta:
+#         db_table = 'reservations'
 
-    def __str__(self):
-        return f"{self.shop.name} {self.date} {self.number_of_people}人 by {self.user}"
+#     def __str__(self):
+#         return f"{self.shop.name} {self.date} {self.number_of_people}人 by {self.user}"
